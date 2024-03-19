38e4e89d3ea1e560bab7f86da240df0243a252a1
limon-chandra-ray <limonroy.19cse013@gmail.com>
2024-03-19 22:15:15 +0600

    'fbq-content-add12'

--- order_server_app/views.py
+++ order_server_app/views.py
@@ -179,16 +179,16 @@ def checkout_gtag(request):
         item['size'] = bitem.product_size
         item['sub_total'] = int(bitem.sub_total)
         items.append(item)
-        fbq_item = dict()
-        fbq_item['content_id'] = bitem.product.p_id
-        fbq_item['content_name'] = bitem.product.p_name
-        fbq_item['value'] = int(bitem.product.p_offer_price)
-        fbq_item['content_type'] = "product"
-        if bitem.product.p_third_category:
-            fbq_item['content_category'] = bitem.product.p_third_category.rc_three_name
-        else:
-            fbq_item['content_category'] = None
-        fbq_items.append(fbq_item)
+        # fbq_item = dict()
+        # fbq_item['content_id'] = bitem.product.p_id
+        # fbq_item['content_name'] = bitem.product.p_name
+        # fbq_item['value'] = int(bitem.product.p_offer_price)
+        # fbq_item['content_type'] = "product"
+        # if bitem.product.p_third_category:
+        #     fbq_item['content_category'] = bitem.product.p_third_category.rc_three_name
+        # else:
+        #     fbq_item['content_category'] = None
+        # fbq_items.append(fbq_item)
 
         
     order_layer = dict()
