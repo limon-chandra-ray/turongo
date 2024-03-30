49b984ce3e37abf040cd50c33d4f0f5f7e3199a5
limon-chandra-ray <limonroy.19cse013@gmail.com>
2024-03-30 19:42:28 +0600

    Change Password option hide3

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ DEBUG = env('DEBUG')
 if DEBUG:
     ALLOWED_HOSTS = []
 else:
-    ALLOWED_HOSTS = ['*',"turong.com","www.turongo.com"]
+    ALLOWED_HOSTS = ['*',"turongo.com","www.turongo.com"]
 
 
 # Application definition
